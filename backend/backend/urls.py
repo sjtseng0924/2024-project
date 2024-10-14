e5b96a3c2232b5a54a9900827d1f1de2cfb1874f
Chu I-Ting <itingchu1005@gmail.com>
2024-10-14 14:20:08 +0800

    backend: modify endpoint url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('chatroom/', include('chatroom.urls')),
+    path('api/chatroom/', include('chatroom.urls')),
     path('admin/', admin.site.urls),
 ]
